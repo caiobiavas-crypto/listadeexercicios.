92efd8e7cc078a2a0eb955ba2af684c308ff6903
caiobiavas-crypto <caio.biavas@gmail.com>
2026-02-27 13:12:04 +0000

    resolucao

---
+++ lista2.js
@@ -0,0 +1,35 @@
+//criar um programa que mostre se o numero e positivo ou negativo
+
+let num1 = 99
+if (num1 > 0) {
+    console.log("O numero é positivo")
+} else if (num1 < 0) {
+    console.log("O numero é negativo")
+} 
+
+
+//criar um programa que mostre se o ano e bissexto 
+let ano = 2020
+if ((ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 === 0)) {
+    console.log("O ano é bissexto")
+} else {
+    console.log("O ano não é bissexto")
+}
+
+
+//criar um programa que mostre uma senha  e a mensagem ascesso permitivo
+
+let senha = "12345"
+if (senha === "12345") {
+  console.log("Acesso permitido")
+}else {  console.log("Acesso negado")
+}
+
+
+//criar um programa que mostre um desconto de 5% para compras maiores de 100 reais
+
+//criar um  programa que clasifique uma pessoa como criança, adolescente, adulto ou idoso de acordo com a idade
+
+//criar um programaque mostre qual tipo de triangulo e de acordo com os lados (equilatero, isosceles ou escaleno)
+
+//criar um programa que mostre conceitos a b c ou d e acordo com as notas: 
